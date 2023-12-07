094d38deda55dc6c7d4baf8e091162bf40d8bebf
dotarjun <arjunsingh8112@gmail.com>
2023-12-07 11:30:25 +0530

    feat: add plug
    Signed-off-by: dotarjun <arjunsingh8112@gmail.com>

--- install.sh
+++ install.sh
@@ -1,1 +1,40 @@
-cp lofi-cli /usr/local/bin  
+echo -e "██╗      ██████╗ ███████╗██╗       ██████╗██╗     ██╗
+██║     ██╔═══██╗██╔════╝██║      ██╔════╝██║     ██║
+██║     ██║   ██║█████╗  ██║█████╗██║     ██║     ██║
+██║     ██║   ██║██╔══╝  ██║╚════╝██║     ██║     ██║
+███████╗╚██████╔╝██║     ██║      ╚██████╗███████╗██║
+╚══════╝ ╚═════╝ ╚═╝     ╚═╝       ╚═════╝╚══════╝╚═╝
+
+Welcome to lofi-cli!
+
+This tool allows you to:
+- Play various audio tracks for relaxation and focus.
+- Choose from different options such as lofi girl, white noise, classical, and ratatouille theme track.
+- Manage custom videos by adding, listing, and playing them.
+
+The tool provides the flexibility to customize your audio experience.
+
+
+Make sure to star the repo on GitHub:
+https://github.com/dotarjun/lofi-cli
+
+Made by:
+      _       _             _             
+   __| | ___ | |_ __ _ _ __(_)_   _ _ __  
+  / _  |/ _ \| __/ _  |  __| | | | |  _ \ 
+ | (_| | (_) | || (_| | |  | | |_| | | | |
+  \__,_|\___/ \__\__,_|_| _/ |\__,_|_| |_|
+                         |__/             
+
+"
+
+
+# sudo cp lofi-cli /usr/local/bin  
+
+if sudo cp lofi-cli /usr/local/bin; then
+    echo -e "\nInstallation successful! You can now run it by using 'lofi-cli'.\n"
+else
+    echo -e "\nInstallation failed. Please check permissions or try again.\n"
+fi
+
+
